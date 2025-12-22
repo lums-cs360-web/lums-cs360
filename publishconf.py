3835aa7a941673aea98c171749575aa7393f8562
SafaSalam <safa16salam@gmail.com>
2025-12-22 10:45:24 +0500

    Slack Link

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://ualberta-cmput301.github.io"
+SITEURL = "https://lums-cs360-web.github.io/lums-cs360/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
